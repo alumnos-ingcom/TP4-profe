9f8ef19f65f137676aac05dc8e5f3e5bb0f22b58
MrTin Vilu <mrvilugron@unrn.edu.ar>
2021-05-11 21:18:57 -0300

    Arreglado un error de sintaxis en el test

--- tp4-ej2.py
+++ tp4-ej2.py
@@ -5,7 +5,7 @@ def suma_lenta(numero, otro_numero):
     assert suma_lenta(10,11) == 20
     
     
-def def test_suma_lenta():
+def test_suma_lenta():
     resultado = suma_lenta(10, 10)
     esperado = 20
     assert resultado == esperado
