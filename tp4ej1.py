8b710d200ff61b71e5e1174d24f3948a198d1b66
MrTin Vilu <mrvilugron@unrn.edu.ar>
2021-05-19 10:38:29 -0300

    ej1 y ej2 finalizados

--- tp4ej1.py
+++ tp4ej1.py
@@ -13,11 +13,11 @@ def ingreso_entero(mensaje):
     Esta funcion muestra un mensaje y agrega la # para indicar el ingreso
     de un número entero.
     """
-    ingreso = input(mensaje + " #")
+    ingreso = input(mensaje + " # ")
     try:
         entero = int(ingreso)
     except ValueError as err:
-        raise IngresoIncorrecto("'{ingreso}' no era un número!") from err
+        raise IngresoIncorrecto(f"'{ingreso}' no era un número!") from err
     return entero
 
 def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
@@ -26,22 +26,38 @@ def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
         try:
             return ingreso_entero(mensaje)
         except IngresoIncorrecto as err:
-            cantidar_reintentos = cantidad_reintentos - 1
-    raise IngresoIncorrecto("Luego de {intentos}")
-    
+            print(f"No era un número, quedan {cantidad_reintentos}")
+            cantidad_reintentos = cantidad_reintentos - 1
+    raise IngresoIncorrecto(f"Luego de {intentos}")
     
     
 def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
-    pass
-    
-
+    msg = f"{mensaje} entre {valor_minimo} y {valor_maximo}"
+    num = ingreso_entero(msg)
+    if (num >= valor_minimo and num <= valor_maximo):
+        return num
+    else:
+        print(f"el ingreso no era un numero entre "
+              f"entre {valor_minimo} y {valor_maximo}")
+        raise IngresoIncorrecto(msg)
+    print("Y acá? que hacemos?")
 
 # Reemplazar por las funciones del ejercicio
 
 
 def prueba():
-    num = ingreso_entero("Dame un número vieja!")
+    msg = "Dame un número vieja!"
+#     num = ingreso_entero(msg)
+#     print(f"El número, vieja; es {num}")
+#     
+#     num = ingreso_entero_reintento(msg, cantidad_reintentos=2)
+#     print(f"El número, vieja; es {num}")
+    
+    num = ingreso_entero_restringido(msg, 2, 8)
     print(f"El número, vieja; es {num}")
+
+    
+print("TP4ej1 " + __name__)
 
 if __name__ == "__main__":
     prueba()
--- tp4ej2.py
+++ tp4ej2.py
@@ -1,11 +1,20 @@
+from tp4ej1 import ingreso_entero, IngresoIncorrecto
+
+
 def suma_lenta(numero, otro_numero):
-    return numero + otro_numero
-
-
-    assert suma_lenta(10,11) == 20
+    return numero + otro_numero    
+    
+def prueba():
+    try:
+        ingreso_entero("pa sumar")
+    except IngresoIncorrecto as oops:
+        print(f"OOOPS {oops}")
     
     
 def test_suma_lenta():
     resultado = suma_lenta(10, 10)
     esperado = 20
-    assert resultado == esperado
+    
+if __name__ == "__main__":
+    print(__name__)
+    prueba()
